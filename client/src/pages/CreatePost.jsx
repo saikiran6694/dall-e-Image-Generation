1eea783dc71fd8fa86bf57ed4d679935e3fba30c
saikiranbembalge <saikiranbembalge123@gmail.com>
2023-03-27 19:21:39 +0530

    updates

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -48,13 +48,16 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch(`http://localhost:9002/api/v1/post`, {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify(form),
-        });
+        const response = await fetch(
+          `https://dall-e-c7rw.onrender.com/api/v1/post`,
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(form),
+          }
+        );
         await response.json();
         navigate("/");
       } catch (error) {
