49d555d6a79b1144ce5a2742708e7fe9a29d62bb
Saikiran <70024114+saikiran6694@users.noreply.github.com>
2023-08-18 00:36:49 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,12 +17,14 @@ const Home = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState(null);
 
+  const base_url = "https://image-gen-sl0e.onrender.com"
+
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
         const response = await fetch(
-          `https://dall-e-c7rw.onrender.com/api/v1/post`,
+          `${base_url}/api/v1/post`,
           {
             method: "GET",
             headers: {
